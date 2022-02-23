a6f51dccdabd3fe7eba0bde5831f7bfe607f185a
Luc Perkins <lucperkins@gmail.com>
2022-02-22 19:08:53 -0800

    Update Nixpkgs SHA

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 let
-  nixpkgsRev = "7f7af7c7796f9d2f523686293de3cf2f149d9aa2"; # Matches flake.nix
+  nixpkgsRev = "6e9efe5d6702cbe24cec6575d46b0660c9dc61ca"; # Matches flake.nix
   pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz") {};
 in pkgs
